cccc5606f65e3c71b25fda44ce7629e17a4bb0ed
Sathish Madem <sathishmadem123@gmail.com>
2026-02-12 09:29:36 +0530

    Integrate AI to generate recipe

--- src/chef-claude/MainContent.jsx
+++ src/chef-claude/MainContent.jsx
@@ -2,10 +2,13 @@ import React from "react"
 import IngredientList from "./IngredientsList";
 import Recipe from "./Recipe";
 
+import { getRecipeFromMistral } from "./ai";
+
 export default function MainContent() {
 
-    const [ingredients, setIngredients] = React.useState([]);
-    const [recipeShown, setRecipeShown] = React.useState(false)
+    const [ingredients, setIngredients] = React.useState([])
+    const [recipe, setRecipe] = React.useState("")
+    const [showLoader, setShowLoader] = React.useState(false)
 
     function addIngredient(formData) {
         const ingredient = formData.get("ingredient")
@@ -14,13 +17,21 @@ export default function MainContent() {
         const exists = ingredients.some(item => item.toLowerCase() === ingredient.toLowerCase())
         if (!exists) {
             setIngredients(prevIngredients => [...prevIngredients, ingredient])
-            setRecipeShown(false)
         }
     }
 
-    function getRecipe() {
-        setRecipeShown(true)
-    }   
+    async function getRecipe() {
+        setShowLoader(true)
+
+        try {
+            const generatedRecipe = await getRecipeFromMistral(ingredients);
+            setRecipe(generatedRecipe)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setShowLoader(false)
+        }
+    }
 
     return (
         <main className="main-container">
@@ -38,8 +49,9 @@ export default function MainContent() {
                     <button className="ingredient-add-button">+ Add ingredient</button>
                 </form>
 
-                {ingredients.length > 0 && <IngredientList ingredients={ingredients} getRecipe={getRecipe}/>}
-                {recipeShown && <Recipe />}
+                {ingredients.length > 0 && <IngredientList ingredients={ingredients} getRecipe={getRecipe} />}
+                {showLoader && <div className="spinner"></div>}
+                {recipe && <Recipe recipe={recipe} />}
             </div>
         </main>
     )
